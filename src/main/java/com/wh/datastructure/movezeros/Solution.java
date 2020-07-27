7f96c2ec18fd925c50b1ecccfbe9f16631f117ca
blackjuly <120026139@qq.com>
2020-07-27 18:08:09 +0800

    feat:准备练习移动0

---
+++ src/main/java/com/wh/datastructure/movezeros/Solution.java
@@ -0,0 +1,20 @@
+package com.wh.datastructure.movezeros;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 28476 WangHao <a href="hao.wang@1hai.cn">Contact me.</a>
+ * @version 1.0
+ * @date 2020/07/27 15:05
+ * desc :
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+
+    }
+
+}
